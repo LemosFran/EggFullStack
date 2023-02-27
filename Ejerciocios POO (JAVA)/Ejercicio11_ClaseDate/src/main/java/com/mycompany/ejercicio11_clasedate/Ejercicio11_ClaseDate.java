94e7945eacb38b6cbe48b9aca13982adcdaefe54
Franco <lemosf199@gmail.com>
2023-02-27 05:06:15 -0300

    quince

---
+++ Ejerciocios POO (JAVA)/Ejercicio11_ClaseDate/src/main/java/com/mycompany/ejercicio11_clasedate/Ejercicio11_ClaseDate.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio11_clasedate;
+
+import java.util.Date;
+
+/**
+ *
+ * @author franc
+ */
+public class Ejercicio11_ClaseDate {
+
+    public static void main(String[] args) {
+        Date date = new Date(); //Se necesita importar
+        Fecha fecha = new Fecha(27, 02, 2023);
+        
+        System.out.println("la fecha es: "+fecha);
+        
+        
+
+        
+    }
+}
